73019efa7def2f7833ed26d20e22095bf81bf13a
Andrey Deriabin <aderiabin.98@gmail.com>
2022-12-10 22:34:33 +0300

    fixed like button

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MyImage from "./UI/img/MyImage";
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import {Fab, Typography} from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BlockIcon from '@mui/icons-material/Block';
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
 import './component-styles/PostItem.css';
 
 
 const PostItem = (props) => {
+    const [isLiked, setIsLiked] = useState(false);
+
     return (
         <div className='post'>
-            <MyImage url={props.post.url}/>
+            <MyImage url={props.post.url} />
 
             <div className="post__btns">
-                <Button variant="contained" color="success" endIcon={<FavoriteIcon/>}>Like</Button>
-                <Button variant="outlined" color="error" onClick={() => props.deletePost(props.post.id)} endIcon={<BlockIcon color="default" />}>Dislike</Button>
-                <div style={{width:'150px'}}></div>
+                <Button
+                    variant={isLiked ? "contained" : "outlined"}
+                    color="success"
+                    endIcon={<FavoriteIcon />}
+                    onClick={() => setIsLiked(!isLiked)}
+                >
+                    Like
+                </Button>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => props.deletePost(props.post.id)}
+                    endIcon={<BlockIcon color="default" />}
+                >
+                    Dislike
+                </Button>
+                <div style={{ width: '150px' }}></div>
                 <Button variant="outlined" endIcon={<ShareOutlinedIcon />}>Share</Button>
             </div>
 
